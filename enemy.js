8719490482f6418ae00cc4a86de8a6ef1c7cc9ff
KimJeongHyun <ptr1129@hanmail.net>
2021-12-01 19:35:46 +0900

    Update js files / ghost interval, hero move applied

--- enemy.js
+++ enemy.js
@@ -1,3 +1,62 @@
 class Enemy {
-  
+    constructor(){
+        this.newElem = document.createElement('div');
+        this.newElem.id = 'enemy';
+        this.newElem.className='enemy'
+        this.y=0;
+        this.interval='';
+    }
+
+    moveBottom(){
+        this.newElem.style.top=this.y+'px';
+    }
+
+    setPosition(){
+        this.newElem.style.backgroundPosition='-45px'
+    }
+
 }
+
+const bg = document.getElementById('bg')
+
+let interval = '';
+
+const startFunc = () =>{
+    document.getElementById('startBtn').style.display='none';
+    document.getElementById('stopBtn').style.display='inline-block';
+    interval = setInterval(function(){
+        const ghost = new Enemy();
+        
+        ghost.newElem.style.left = Math.random()*765+'px'
+        
+        const moveBottomInterval = setInterval(function(){
+            if (ghost.y<=540){
+                ghost.moveBottom();
+                ghost.y+=180;
+            }else{
+                ghost.setPosition();
+            }
+        },800)
+
+        if (ghost.y>=540){
+            clearInterval(moveBottomInterval);
+            ghost.setPosition();
+        }
+
+        bg.appendChild(ghost.newElem);
+    },1500)
+}
+
+const stopFunc = () =>{
+    clearInterval(interval);
+    const enemies = document.getElementsByClassName('enemy')
+    while(enemies.length>0){
+        for (let i=0; i<enemies.length; i++){
+            bg.removeChild(enemies[i]);
+        }
+    }
+
+    document.getElementById('stopBtn').style.display='none';
+    document.getElementById('startBtn').style.display='inline-block';
+}
+
--- hero.js
+++ hero.js
@@ -1,3 +1,55 @@
 class Hero {
-  
+    constructor(){
+        this.hero=document.getElementById('hero');
+        this.x = 400;
+        this.y = 540;
+    }
+    moveTop(){
+        this.hero.style.backgroundPosition='-35px';
+        this.hero.style.top = this.y+'px';
+    }
+    moveBottom(){
+        this.hero.style.backgroundPosition='0px';
+        this.hero.style.top = this.y+'px'
+    }
+    moveLeft(){
+        this.hero.style.backgroundPosition='70px';
+        this.hero.style.left = this.x+'px';
+    }
+    moveRight(){
+        this.hero.style.backgroundPosition='35px';
+        this.hero.style.left = this.x+'px';
+    }
+    
 }
+
+const hero = new Hero();
+
+window.addEventListener('keydown',(e)=>{
+    switch(e.key){
+        case 'ArrowRight':
+            if (hero.x<bg.offsetWidth-35){
+                hero.moveRight()
+                hero.x+=5;
+            }
+            break;
+        case 'ArrowLeft':
+            if (hero.x>0){
+                hero.moveLeft()
+                hero.x-=5;
+            }
+            break;
+        case 'ArrowUp':
+            if (hero.y>0){
+                hero.moveTop();
+                hero.y-=5;
+            }
+            break;
+        case 'ArrowDown':
+            if (hero.y<=540){
+                hero.moveBottom();
+                hero.y+=5;
+            }
+            break;
+    }
+})
